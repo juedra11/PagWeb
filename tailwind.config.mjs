e5fea7a0a702b6bcc9d1d3ada5aa1b4ef5b85ae9
juedra11 <juan1juan.jdar@gmail.com>
2024-12-29 12:34:03 -0500

    Primer commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+
+
+			fontFamily:{
+				lato:['Lato','sans-serif'],
+				Audio:['Audiowide','sans-serif'],
+
+			}
+		},
+	},
+	plugins: [],
+}
